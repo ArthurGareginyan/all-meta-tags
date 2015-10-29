fdd2e45dfa841abe481d18ccb253f52b430f1182
Arthur Gareginyan <arthurgareginyan@gmail.com>
2015-10-29 14:58:12 +0300

    First commit. First stable release.

---
+++ inc/settings_page.php
@@ -0,0 +1,230 @@
+<?php
+
+/**
+ * Prevent Direct Access
+ */
+defined('ABSPATH') or die("Restricted access!");
+
+/**
+ * Render Settings Page
+ *
+ * @since 1.0
+ */
+function allmetatags_render_submenu_page() {
+
+	// Declare variables
+    $options = get_option( 'allmetatags_settings' );
+
+	// Settings update message
+	if ( isset( $_GET['settings-updated'] ) ) :
+		?>
+			<div id="message" class="updated">
+				<p>
+					<?php _e( 'Your Meta Tags was sucessfully updated.', 'allmetatags' ); ?>
+				</p>
+			</div>
+		<?php
+	endif;
+
+	// Page
+	?>
+	<div class="wrap">
+		<h2>
+            <?php _e( 'All Meta Tags', 'allmetatags' ); ?>
+            <br/>
+            <span>
+                <?php _e( 'by <a href="http://mycyberuniverse.com/author.html" target="_blank">Arthur "Berserkr" Gareginyan</a>', 'allmetatags' ); ?>
+            <span/>
+		</h2>
+
+        <div id="poststuff" class="metabox-holder has-right-sidebar">
+
+            <!-- SIDEBAR -->
+            <div class="inner-sidebar">
+                <div id="side-sortables" class="meta-box-sortabless ui-sortable">
+
+                    <div id="about" class="postbox">
+                        <h3 class="title">About</a></h3>
+                        <div class="inside">
+                            <p class="about">
+                                <img src="<?php echo plugins_url('thanks.png', __FILE__); ?>">
+                            </p>
+                            <p>
+                                <?php _e( 'This plugin allows you to easily add Meta Tags to your website.', 'allmetatags' ) ?>
+                            </p>
+                            <p>
+                                <?php _e( 'To use, enter your custom Meta Tags, then click "Save Changes". It\'s that simple!', 'allmetatags' ) ?>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div id="donate" class="postbox">
+                        <h3 class="title">Donate</h3>
+                        <div class="inside">
+                            <p class="donate">If you like this plugin and find it useful, help me to make this plugin even better and keep it up-to-date.</p>
+                            <div class="aligncenter">
+                                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8A88KC7TFF6CS" target="_blank" rel="nofollow">
+                                    <img src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" alt="Make a donation">
+                                </a>
+                            </div>
+                            <p class="donate">Thanks for your support!</p>
+                        </div>
+                    </div>
+
+                    <div id="help" class="postbox">
+                        <h3 class="title">Help</h3>
+                        <div class="inside">
+                            <div class="aligncenter">
+                                <p>If you want more options then tell me and I will be happy to add it.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- END-SIDEBAR -->
+
+            <!-- FORM -->
+            <div class="has-sidebar sm-padded">
+                <div id="post-body-content" class="has-sidebar-content">
+                    <div class="meta-box-sortabless">
+
+                        <form name="allmetatags-form" action="options.php" method="post" enctype="multipart/form-data">
+                            <?php settings_fields( 'allmetatags_settings_group' ); ?>
+
+                            <?php do_action( 'allmetatags-form-top' ); ?>
+
+                            <div class="postbox" id="WebMasterTools">
+                                <h3 class="title">Web Master Tools</h3>
+                                <div class="inside">
+                                    <p>Webmaster Tools require you to verify your domain. This makes sure that you are the correct owner of your blog or store before they provide their services to you. You can use the options below to verify your domain. If your domain is already verified, you can just forget about these.</p>
+                                    <table class="form-table">
+                                        <?php allmetatags_field('google',
+                                                                'Google Webmaster Tools',
+                                                                '1234567890',
+                                                                'Enter your meta key “content” value from your verification code to verify your website. </br>Example: &lt;meta name="google-site-verification" content=“<b>1234567890</b>” /&gt;',
+                                                                'https://www.google.com/webmasters/verification/');?>
+                                        <?php allmetatags_field('bing',
+                                                                'Bing Webmaster Tools',
+                                                                '1234567890',
+                                                                'Enter your meta key “content” value from your verification code to verify your website. </br>Example: &lt;meta name="msvalidate.01" content=“<b>1234567890</b>” /&gt;',
+                                                                'http://www.bing.com/webmaster/');?>
+                                        <?php allmetatags_field('yandex',
+                                                                'Yandex Webmaster Tools',
+                                                                '1234567890',
+                                                                'Enter your meta key “content” value from your verification code to verify your website. </br>Example: &lt;meta name="yandex-verification" content=“<b>1234567890</b>” /&gt;',
+                                                                'https://webmaster.yandex.com');?>
+                                    </table>
+                                    <?php submit_button( __( 'Save Changes', 'allmetatags' ), 'primary', 'submit', true ); ?>
+                                </div>
+                            </div>
+
+                            <div class="postbox" id="DomainVerification">
+                                <h3 class="title">Domain Verification</h3>
+                                <div class="inside">
+                                    <p>Third-party services like Alexa, Pinterest and Google+ require you to verify your domain. This makes sure that you are the correct owner of your blog or store before they provide their services to you. You can use the options below to verify your domain. If your domain is already verified, you can just forget about these.</p>
+                                    <table class="form-table">
+                                        <?php allmetatags_field('pinterest',
+                                                                'Pinterest',
+                                                                '1234567890',
+                                                                'Enter your meta key “content” value from your verification code to verify your website. </br>Example: &lt;meta name="pinterest_webmaster" content=“<b>1234567890</b>” /&gt;',
+                                                                'https://help.pinterest.com/en/articles/confirm-your-website');?>
+                                        <?php allmetatags_field('google_author',
+                                                                'Google+',
+                                                                'https://plus.google.com/+ArthurGareginyan/',
+                                                                'Enter an absolute URL to the Google+ profile of the publisher. </br>Example: https://plus.google.com/+ArthurGareginyan/',
+                                                                'https://plus.google.com/');?>
+                                        <?php allmetatags_field('facebook',
+                                                                'Facebook',
+                                                                'https://www.facebook.com/arthur.gareginyan',
+                                                                'Enter an absolute URL to the Facebook profile of the publisher. </br>Example: https://www.facebook.com/arthur.gareginyan',
+                                                                'https://www.facebook.com/');?>
+                                        <?php allmetatags_field('twitter',
+                                                                'Twitter',
+                                                                '@AGareginyan',
+                                                                'Enter the Twitter username of the publisher. </br>Example: @AGareginyan',
+                                                                'https://twitter.com/');?>
+                                        <?php allmetatags_field('alexa',
+                                                                'Alexa',
+                                                                '1234567890',
+                                                                'Enter your meta key “content” value from your verification code to verify your website. </br>Example: &lt;meta name="alexaVerifyID" content=“<b>1234567890</b>” /&gt;',
+                                                                'http://www.alexa.com/siteowners/claim');?>
+                                        <?php allmetatags_field('norton',
+                                                                'Norton Safe Web',
+                                                                '1234567890',
+                                                                'Enter your meta key “content” value from your verification code to verify your website. </br>Example: &lt;meta name="norton-safeweb-site-verification" content=“<b>1234567890</b>” /&gt;',
+                                                                'https://safeweb.norton.com/help/site_owners');?>
+                                    </table>
+                                    <?php submit_button( __( 'Save Changes', 'allmetatags' ), 'primary', 'submit', true ); ?>
+                                </div>
+                            </div>
+
+                            <div class="postbox" id="Home">
+                                <h3 class="title">Meta Tags for Home Page only</h3>
+                                <div class="inside">
+                                    <p>You can use the options below to add meta tags such as Description and Keywords only in Home Page of your website.</p>
+                                    <table class="form-table">
+                                        <?php allmetatags_field('home_description',
+                                                                'Home Description',
+                                                                'My blog is about plants, nature, the sea and everything I love',
+                                                                'Enter a short description of your website (150-250 characters). Most search engines use a maximum of 160 chars for the home description.',
+                                                                '',
+                                                                'textarea');?>
+                                        <?php allmetatags_field('home_keywords',
+                                                                'Home Keyword(s)',
+                                                                'blog, awesome, handmade, books, theater',
+                                                                'Enter a comma-delimited list of keywords for only Home page of your website.',
+                                                                '',
+                                                                'textarea');?>
+                                    </table>
+                                    <?php submit_button( __( 'Save Changes', 'allmetatags' ), 'primary', 'submit', true ); ?>
+                                </div>
+                            </div>
+
+                            <div class="postbox" id="Everywhere">
+                                <h3 class="title">Meta Tags for all website (Global)</h3>
+                                <div class="inside">
+                                    <p>You can use the options below to add meta tags such as Author, Copyright and Keywords in everywhere on your website.</p>
+                                    <table class="form-table">
+                                        <?php allmetatags_field('author',
+                                                                'Author',
+                                                                'Arthur Gareginyan',
+                                                                '',
+                                                                '');?>
+                                        <?php allmetatags_field('designer',
+                                                                'Designer',
+                                                                'Arthur Gareginyan',
+                                                                '',
+                                                                '');?>
+                                        <?php allmetatags_field('contact',
+                                                                'Contact',
+                                                                'arthurgareginyan@gmail.com',
+                                                                '');?>
+                                        <?php allmetatags_field('copyright',
+                                                                'Copyright',
+                                                                'Copyright (c) 2013-2015 Arthur Berserkr Gareginyan. All Rights Reserved.',
+                                                                '');?>
+                                        <?php allmetatags_field('keywords',
+                                                                'Keyword(s)',
+                                                                'blog, awesome, handmade, books, theater',
+                                                                'Enter a comma-delimited list of global keywords for your website.',
+                                                                '',
+                                                                'textarea');?>
+                                    </table>
+                                    <?php submit_button( __( 'Save Changes', 'allmetatags' ), 'primary', 'submit', true ); ?>
+                                </div>
+                            </div>
+
+                            <?php do_action( 'allmetatags-form-bottom' ); ?>
+                        </div>
+                    </div>
+                </div>
+                <!-- END-FORM -->
+
+            </form>
+
+        </div>
+
+	</div>
+	<?php
+}
---
+++ readme.txt
@@ -0,0 +1,154 @@
+=== All Meta Tags ===
+Contributors: Arthur Gareginyan
+Tags: meta, tag,tags, custom, simple, plugin, twitter, facebook, g+, google, google+, google plus, meta, search engine optimization, seo, social, alexa, alexa rank, pinterest, norton safe web, webmaster, webmaster tool, webmaster tools, web master tools, web master tools, bing, yandex, bing webmaster, yandex webmaster, google webmaster, web tools, open graph, publisher profile, publisher, author, description, keyword, keywords, copyright, designer, admin, meta tag, pinterest, pinterest meta tag, pinterest website verification, pinterest verification, pinterest verify, plugin, verification, verify, webmaster plugin,
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8A88KC7TFF6CS
+Requires at least: 3.9
+Tested up to: 4.3
+Stable tag: 1.0
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+EASILY add your Meta Tags to the WordPress website's head section.
+
+== Description ==
+An easy to use WordPress plugin that gives you the ability to EASILY add your Meta Tags to the head section of your website. 
+
+No need anymore to editing a files of your theme or plugins in order to add meta tags. You can add they on plugins settings page. Just add your ID (the meta key “content” value from your verification code) or/and the URL to the social media profile of the publisher in the field on the plugin page and this plugin will do the rest for you. It adds required meta tags to the head section of your website automatically, without changing any of your themes file and without slowing down your website. It's really useful in case of any theme update, because your meta tags would never be lost! Your meta tags will keep on working, no matter how many times you upgrade or switch your theme and plugins.
+
+Third-party services like Google Webmaster Tools, Alexa, Pinterest and Google+ require you to verify your domain. This makes sure that you are the correct owner of your blog or store before they provide their services to you. You can use this plugin to easily verify your website or domain and get a more effective and efficient sharing results. Example with Pinterest. Once you completed the verification process, people will see a checkmark next to your domain in your Pinterest profile and in pinner search results. That check mark emphasis you have confirmed the ownership of your blog or website on Pinterest. This will help your website to rank better in google and other search engines. You can easily increase your blog traffic using this plugin. “All Meta Tags” is a simple but effective SEO plugin.
+
+Also you can use this plugin to add required meta tags such as Author, Designer, Contact, Copyright and Keywords in everywhere on your website (It adds the required meta tags to the header of all pages on your website) or the meta Description and Keywords only for Home Page.
+
+
+= Features =
+
+**Current features:**
+
+* Google+ publisher link.
+* Facebook publisher meta tag.
+* Twitter publisher meta tag.
+* Pinterest verification meta tag.
+* Alexa verification meta tag.
+* Norton Safe Web verification meta tag.
+* Google Web Master Tools verification meta tag.
+* Bing Web Master Tools verification meta tag.
+* Yandex Web Master Tools verification meta tag.
+* Meta tag with name="author".
+* Meta tag with name="designer".
+* Meta tag with name="contact".
+* Meta tag with name="copyright".
+* Meta tag with name="description" (Global and only for home page).
+* Ready for translation (.pot file included).
+* Added russian translation.
+
+
+**Coming soon:**
+
+* Google Analytics verification meta tag.
+* SpecificFeeds.com verification meta tag.
+* Meta tag with name="creator".
+* Meta tag with name="publisher".
+* Meta tag with name="language".
+* (Got more ideas? Tell me!)
+
+For more details please visit - [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-all-meta-tags.html)
+
+**Please Vote and Enjoy**
+
+* Your votes really make a difference! Thanks.
+
+== Installation ==
+Install "All Meta Tags” just as you would any other WP Plugin.
+
+Automatically via WordPress:
+
+1. Log into WordPress Dashboard of your website.
+2. Go to "Plugins" —> “add new plugins".
+3. Find this plugin and click install.
+4. Activate this plugin through the "Plugins" tab.
+
+Manual via FTP:
+
+1. Download a copy (zip file) of this plugin from WordPress.org.
+2. Unzip the zip file.
+3. Upload the unzipped directory to your website's plugin directory (/wp-content/plugins/).
+4. Log into WordPress Dashboard of your website.
+5. Activate this plugin through the "Plugins" tab.
+
+After installation, a “All Meta Tags” menu item will appear in the “Settings” section. Click on this in order to view plugin's administration page.
+
+[More help installing Plugins](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins "WordPress Codex: Installing Plugins")
+
+
+== Frequently Asked Questions ==
+= Will this Plugin work on my WordPress.com website? =
+Sorry, this plugin is available for use only on self-hosted (WordPress.org) websites.
+
+= Can I use this plugin on my language? =
+Yes. But If your language is not available then you can make one. This plugin is ready for translation. The .pot file is included and placed in "languages" folder. Many of plugin users would be delighted if you shared your translation with the community. Just send the translation files (*.po, *.mo) to me at the arthurgareginyan@gmail.com and I will include the translation within the next plugin update.
+
+= How does it work? =
+First, go to page “Settings” —> “All Meta Tags”.
+On the plugin page you find the fields where you can enter your ID (meta key “content” value from your verification code), URL to to the your social network profile or name of your social network profile.
+ to verify your website.
+
+= How much of description I can enter in the text field? =
+I don't limit the number of characters, but most search engines use a maximum of 160 chars for the home description.
+
+= How much of keywords I can enter in the text field? =
+I don't limit the number of characters.
+
+= Where to report bug if found? =
+Please visit [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-all-meta-tags.html) and report.
+
+= Where to share any ideas or suggestions to make the plugin better? =
+Please visit [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-all-meta-tags.html) and share.
+
+
+== Screenshots ==
+1. Plugin’s page.
+
+
+== Other Notes ==
+
+“All Meta Tags” is one of the personal software projects of Arthur Gareginyan.
+
+**License**
+
+This plugin is licensed under the [GNU General Public License, version 3 (GPLv3)](http://www.gnu.org/licenses/gpl-3.0.html)
+and is distributed free of charge.
+Commercial licensing (e.g. for projects that can’t use an open-source license) is available upon request.
+
+**Support**
+
+* Did you enjoy this plugin? Please [donate to support ongoing development](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8A88KC7TFF6CS). Your contribution would be greatly appreciated.
+* Do you have any ideas or suggestions to make the plugin better? I can’t wait to hear them! Please share! [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-all-meta-tags.html)
+
+**Please Vote and Enjoy**
+
+* Your votes really make a difference! Thanks.
+
+**Links**
+
+* [Developer Website](http://mycyberuniverse.com)
+* [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-all-meta-tags.html)
+
+
+== Changelog ==
+= 1.0 =
+* Initial release.
+* Added ready for translation (.pot file included).
+* Added russian translation.
+= 0.2 =
+* Beta version.
+= 0.1 =
+* Alfa version.
+
+
+== Upgrade Notice ==
+= 1.0 =
+Please update to first stable release!
+* Added ready for translation (.pot file included).
+* Added russian translation.
+= 0.2 =
+Please update to beta version.
