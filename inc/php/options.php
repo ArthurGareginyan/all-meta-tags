7a3e02fbddaad3b423307432c3bed0c53521a03d
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-03-08 14:34:34 +0300

    Maintenance: Code formatting improved

--- inc/php/options.php
+++ inc/php/options.php
@@ -22,54 +22,54 @@ function spacexchimp_p004_options() {
 
     // Set default value if option is empty
     $list = array(
-        'hidden_scrollto' => '0',
-        'google' => '',
-        'bing' => '',
-        'yandex' => '',
         'alexa' => '',
-        'pinterest' => '',
-        'google_author' => '',
-        'facebook' => '',
-        'twitter' => '',
-        'norton' => '',
-        'wot' => '',
-        'specificfeeds' => '',
-        'custom_meta' => '',
-        'home_description' => '',
-        'home_keywords' => '',
+        'author' => '',
+        'bing' => '',
         'blog_description' => '',
         'blog_keywords' => '',
-        'author' => '',
-        'designer' => '',
         'contact' => '',
         'copyright' => '',
+        'custom_meta' => '',
+        'designer' => '',
+        'facebook' => '',
+        'google_author' => '',
+        'google' => '',
+        'hidden_scrollto' => '0',
+        'home_description' => '',
+        'home_keywords' => '',
         'keywords' => '',
+        'norton' => '',
+        'pinterest' => '',
+        'specificfeeds' => '',
+        'twitter' => '',
+        'wot' => '',
+        'yandex' => '',
     );
     foreach ( $list as $name => $default ) {
         $array[$name] = !empty( $options[$name] ) ? $options[$name] : $default;
     }
 
     // Sanitize data
-    $array['google'] = esc_textarea( $array['google'] );
-    $array['bing'] = esc_textarea( $array['bing'] );
-    $array['yandex'] = esc_textarea( $array['yandex'] );
     $array['alexa'] = esc_textarea( $array['alexa'] );
-    $array['pinterest'] = esc_textarea( $array['pinterest'] );
-    $array['google_author'] = esc_textarea( $array['google_author'] );
-    $array['facebook'] = esc_textarea( $array['facebook'] );
-    $array['twitter'] = esc_textarea( $array['twitter'] );
-    $array['norton'] = esc_textarea( $array['norton'] );
-    $array['wot'] = esc_textarea( $array['wot'] );
-    $array['specificfeeds'] = esc_textarea( $array['specificfeeds'] );
-    $array['home_description'] = esc_textarea( $array['home_description'] );
-    $array['home_keywords'] = esc_textarea( $array['home_keywords'] );
+    $array['author'] = esc_textarea( $array['author'] );
+    $array['bing'] = esc_textarea( $array['bing'] );
     $array['blog_description'] = esc_textarea( $array['blog_description'] );
     $array['blog_keywords'] = esc_textarea( $array['blog_keywords'] );
-    $array['author'] = esc_textarea( $array['author'] );
-    $array['designer'] = esc_textarea( $array['designer'] );
     $array['contact'] = esc_textarea( $array['contact'] );
     $array['copyright'] = esc_textarea( $array['copyright'] );
+    $array['designer'] = esc_textarea( $array['designer'] );
+    $array['facebook'] = esc_textarea( $array['facebook'] );
+    $array['google_author'] = esc_textarea( $array['google_author'] );
+    $array['google'] = esc_textarea( $array['google'] );
+    $array['home_description'] = esc_textarea( $array['home_description'] );
+    $array['home_keywords'] = esc_textarea( $array['home_keywords'] );
     $array['keywords'] = esc_textarea( $array['keywords'] );
+    $array['norton'] = esc_textarea( $array['norton'] );
+    $array['pinterest'] = esc_textarea( $array['pinterest'] );
+    $array['specificfeeds'] = esc_textarea( $array['specificfeeds'] );
+    $array['twitter'] = esc_textarea( $array['twitter'] );
+    $array['wot'] = esc_textarea( $array['wot'] );
+    $array['yandex'] = esc_textarea( $array['yandex'] );
 
     // Modify data
 
