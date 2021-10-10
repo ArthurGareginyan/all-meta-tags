f89a8e1f38d02e430848baaa86f473d486b4bd6a
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-10-11 01:33:45 +0300

    Maintenance: Code commenting improved

--- inc/php/options.php
+++ inc/php/options.php
@@ -14,15 +14,15 @@ function spacexchimp_p004_options() {
     // Put value of plugin constants into an array for easier access
     $plugin = spacexchimp_p004_plugin();
 
-    // Retrieve options from database
+    // Retrieve the plugin options data from the database
     $array = get_option( $plugin['settings'] . '_settings' );
 
-    // Make the "$options" array if the plugin options data in the database is not exist
+    // Fill in the "$array" variable if the plugin options data in the database is not exist
     if ( ! is_array( $array ) ) {
         $array = array();
     }
 
-    // Create an array with options
+    // Prepare the plugin options data for use
     $list = array(
         'alexa' => (string) '', // _control_field
         'author' => (string) '', // _control_field
